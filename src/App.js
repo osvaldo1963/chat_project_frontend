51d7fb07360dbf0e1c98fdcaa0697f15ea8c6cae
osvaldo <osvaldo@fleet55.com>
2022-03-01 19:40:22 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Peer from 'simple-peer';
+import React, { useEffect, useRef, useState } from "react"
 function App() {
+  const [ stream, setStream ] = useState();
+  const myVideo = useRef()
+  
+  useEffect(() => {
+    navigator.mediaDevices.getUserMedia({
+      video: true, 
+      audio: true
+    }).then((stream) => {
+      const peer1 = new Peer({initiator: true});
+      peer1.on('signal', (data) => {
+        setStream(data)
+        myVideo.current.srcObject = stream
+      });
+     
+    }).catch((error) => {
+      console.log(error);
+    })
+  });
+  
+
   return (
     <div className="App">
       <header className="App-header">
@@ -16,6 +37,7 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
+          {stream &&  <video playsInline muted ref={myVideo} autoPlay style={{ width: "700px" }} />}
         </a>
       </header>
     </div>
