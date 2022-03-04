6d98bff4849510d39d92bccd9d2e410884bb5573
osvaldo <osvaldo@fleet55.com>
2022-03-03 17:41:33 -0800

    peer and socket work in progress

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 import Peer from 'simple-peer';
-import React, { useEffect, useRef, useState } from "react"
+import React, { useEffect, useRef, useState } from "react";
+import Socket from 'socket.io-client';
+
+const ENDPOINT = "http://localhost:3005";
 function App() {
+
   const [ stream, setStream ] = useState();
+  const [me, setMe] = useState();
+  const [call, setCall] = useState();
+  const [callAccepted, setCallAccepted] = useState();
+  const [callEnded, setCallEnded] = useState();
+
   const myVideo = useRef()
-  
+
   useEffect(() => {
     navigator.mediaDevices.getUserMedia({
       video: true, 
@@ -16,27 +24,43 @@ function App() {
         setStream(data)
         myVideo.current.srcObject = stream
       });
-     
     }).catch((error) => {
       console.log(error);
     })
-  });
+    const socket = Socket(ENDPOINT);
+    socket.on("me", (id) => setMe(id));
+    socket.on('calluser', (callinfo) => {
+      const { from, name: callerName, signal } = callinfo;
+      setCall({ isReceivedCall: true, from, name: callerName, signal });
+    });
+
+  }, []);
   
+  const answerCall = () => {
+    setCallAccepted(true);
+    const peer = new Peer({initiator: false, trickle: false, stream});
+    peer.on('signal', (data) => {
+      Socket.emit('answercall', { signal: data, to: call.from });
+    })
+  }
+
+  const callUser = () => {
+
+  }
+
+  const leaveCall = () => {
+
+  }
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
           {stream &&  <video playsInline muted ref={myVideo} autoPlay style={{ width: "700px" }} />}
         </a>
       </header>
